4df06fcbc4f7f7e629bc7d5f87dae144e0e49c4c
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-01-31 00:42:27 +0200

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,22 +1,22 @@
-def caching_fibonacci():
-    cache = {}
+def caching_fibonacci():  # ФУНКЦІЯ caching_fibonacci
+    cache = {}  # Створити порожній словник cache
 
     def fibonacci(n):
-        if n <= 0:
+        if n <= 0: # Якщо n <= 0, повернути 0
             return 0
-        elif n == 1:
+        elif n == 1:  # Якщо n == 1, повернути 1
             return 1
-        elif n in cache:
+        elif n in cache:  # Якщо n у cache, повернути cache[n]
             return cache[n]
         else:
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-            return cache[n]
+            return cache[n]  # Повернути cache[n]
 
     return fibonacci
 
+
 # Приклад використання
 fib = caching_fibonacci()
-print(fib(5))  # Виведе 5 (0, 1, 1, 2, 3, 5)
-print(fib(10))
-print(fib(15))
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
 print([fib(x) for x in range(16)])
