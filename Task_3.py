f5573f2371d8b2beb4bf05fae239a4e45b3d1e53
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-02-02 17:03:15 +0200

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,3 +1,4 @@
+import sys
 from collections import defaultdict
 
 def parse_log_line(line: str) -> dict:
@@ -45,14 +46,9 @@ def main(file_path: str, log_level: str = None):
     print(" ")
 
 if __name__ == "__main__":
-    import sys
     if len(sys.argv) < 2:
         print("Використання: python script.py path/to/logfile.log [log_level]")
         sys.exit(1)
     file_path = sys.argv[1]
     log_level = sys.argv[2] if len(sys.argv) > 2 else None
     main(file_path, log_level)
-        display_log_counts(counts)
-    else:
-        counts = count_logs_by_level(logs)
-        display_log_counts(counts)
